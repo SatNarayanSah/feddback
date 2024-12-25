3caa65164ba6078e44391f983e95213679d669ea
Satyam Sah <satnarayan5166@gmail.com>
2024-12-26 03:44:58 +0545

    sign page working

--- src/middleware.ts
+++ src/middleware.ts
@@ -18,17 +18,17 @@ export async function middleware(request: NextRequest) {
         return NextResponse.redirect(new URL('/dashboard', request.url))
     }
 
-    return NextResponse.redirect(new URL('/home', request.url))
+    // return NextResponse.redirect(new URL('/', request.url))
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
     matcher: [
-        '/sing-in',
-        'sign-up',
+        '/sign-in',
+        '/sign-up',
         '/',
-        'dashboard/path*',
-        'verify/:path*'
+        // '/dashboard/path*',
+        // '/verify/:path*'
 
     ]
 }
