209311e882a3062bfb35307c0c8dcfd73120f9ee
Satyam Sah <satnarayan5166@gmail.com>
2024-12-25 18:46:42 +0545

    database connection written

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+// import { number } from "zod";
+
+
+type ConnectionObject = {
+    isConnected?: number
+}
+
+const connections: ConnectionObject = {}
+
+async function dbConnect(): Promise<void> {
+    if (connections.isConnected) {
+        console.log("Already Connected to datase");
+        return
+
+    }
+    try {
+        const db = await mongoose.connect(process.env.MONGOSB_URI || '', {})
+        connections.isConnected = db.connections[0].readyState
+        console.log("Database connected successfully")
+    } catch (error) {
+        console.log("database connection failed",error)
+        process.exit(1)
+    }
+}
+export default dbConnect
